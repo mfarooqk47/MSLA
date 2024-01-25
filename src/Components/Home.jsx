bff1760c51694da520291f5d246992e0be866665
MUHAMMAD FAROOQ KHAN <75903910+mfarooqk47@users.noreply.github.com>
2024-01-25 16:09:27 +0500

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@
  */
 
 import React from "react";
-import arrowSvg from "../images/down-arrow.svg";
+import arrowSvg from "mountains.jpg";
 import PropTypes from "prop-types";
 
 /**
@@ -22,7 +22,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/woman-with-tablet.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Muhammad Farooq Khan - Project Manager and Data Analyst. This is background photo.";
 
 const Home = ({ name, title }) => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "castle.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "black castle image in mountains";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Act of kindness🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "My blog on Medium.com regarding the Act of kindness.",
+    url: "https://medium.com/@muhammadfarooqk/act-of-kindness-375fc09d6f4c",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Photo Journal",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "My Photo Journal while I was a fellow",
+    url: "https://medium.com/@muhammadfarooqk/photo-journal-4f0ba83c1971",
   },
   {
-    title: "My Resume Site",
+    title: "My Resume",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Here is my resume that showcase the skills and past expreience that I gained in the previous years.",
+    url: "https://drive.google.com/file/d/1fnDVY8d_aopAQQU_wdai9kMPEFrbAQlF/view?usp=drive_link",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Hope That Drives",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "I wrote a short story on the importance of hope",
+    url: "https://drive.google.com/file/d/1SNe7z6muxU07L2h_3JRdUopTLMIQzdu0/view?usp=sharing",
   },
 ];
 
